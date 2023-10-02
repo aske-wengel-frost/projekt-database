24bb0a0e49b0d92301852d6090c7bd5bfc7b3450
askewengelfrost <askefrost1212@gmail.com>
2023-10-02 18:38:31 +0200

    Fixed issues related to showing data with 'tk.Treeview'. And further more added ID's to respective boilerplate-data.

--- data/bookData.py
+++ data/bookData.py
@@ -4,35 +4,25 @@ con = sqlite3.connect("library-data.db")
 cur = con.cursor()
 
 # Define table
-# cur.execute("CREATE TABLE books(author, yearpublished, title, genre, state)")
+cur.execute("CREATE TABLE books(id, author, yearpublished, title, genre, state)")
 
 # Data insertion
 # First define Data
 data = [
-    ("Harper Lee", 1960, "To Kill a Mockingbird", "Fiction", True),
-    ("J.K. Rowling", 1997, "Harry Potter and the Philosopher's Stone", "Fantasy", True),
-    ("George Orwell", 1949, "1984", "Dystopian", True),
-    ("Jane Austen", 1813, "Pride and Prejudice", "Romance", False),
+    (1, "Harper Lee", 1960, "To Kill a Mockingbird", "Fiction", True),
+    (2, "J.K. Rowling", 1997, "Harry Potter and the Philosopher's Stone", "Fantasy", True),
+    (3, "George Orwell", 1949, "1984", "Dystopian", True),
+    (4, "Jane Austen", 1813, "Pride and Prejudice", "Romance", False),
     (
-        "Gabriel García Márquez",
-        1967,
-        "One Hundred Years of Solitude",
-        "Magical Realism",
-        True,
+        5, "Gabriel García Márquez",
+        1967, "One Hundred Years of Solitude", "Magical Realism", True,
     ),
-    ("Agatha Christie", 1934, "Murder on the Orient Express", "Mystery", True),
-    ("F. Scott Fitzgerald", 1925, "The Great Gatsby", "Classic", False),
-    ("J.R.R. Tolkien", 1954, "The Lord of the Rings", "Fantasy", True),
-    ("Chimamanda Ngozi Adichie", 2003, "Purple Hibiscus", "Fiction", False),
-    ("Khaled Hosseini", 2003, "The Kite Runner", "Historical Fiction", True),
+    (6, "Agatha Christie", 1934, "Murder on the Orient Express", "Mystery", True),
+    (7, "F. Scott Fitzgerald", 1925, "The Great Gatsby", "Classic", False),
+    (8, "J.R.R. Tolkien", 1954, "The Lord of the Rings", "Fantasy", True),
+    (9, "Chimamanda Ngozi Adichie", 2003, "Purple Hibiscus", "Fiction", False),
+    (10, "Khaled Hosseini", 2003, "The Kite Runner", "Historical Fiction", True),
 ]
-cur.executemany("INSERT INTO books VALUES(?,?,?,?,?)", data)
+
+cur.executemany('INSERT INTO books VALUES(?,?,?,?,?,?)', data)
 con.commit()  # Commit transaction after excuting INSERT
-
-
-def fetch_data():
-    # Fetch Data
-    for row in cur.execute(
-        "SELECT author,yearpublished, title, genre, state FROM books"
-    ):
-        return [row]
--- data/libraryGui.py
+++ data/libraryGui.py
@@ -1,30 +1,46 @@
-from sqlite3 import enable_callback_tracebacks
 import tkinter as tk
 from tkinter import ttk
-from bookData import fetch_data
+import sqlite3
 
+
+def connect_to_db():
+    con1 = sqlite3.connect("library-data.db")
+    cur1 = con1.cursor()
+    cur1.execute("CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, author, yearpublished, title, genre, state)")
+    con1.commit()
+    con1.close()
+
+
+def view_data():
+    con1 = sqlite3.connect("library-data.db")
+    cur1 = con1.cursor()
+    cur1.execute("SELECT * FROM books")
+    rows = cur1.fetchall()
+    for row in rows:
+        print(row)
+        tree.insert("", tk.END, value=row)
+    con1.close()
+
+
+# Connect to database
+connect_to_db()
 root = tk.Tk()
 root.title("Library Data")
+root.geometry("1400x800")
 
 # Create and pack the frame separately
 table_frame = ttk.Frame(root)
 table_frame.pack()
 
-table = ttk.Treeview(table_frame)
-table["columns"] = ("ID", "Author", "Year Published", "Title", "Genre", "State")
+tree = ttk.Treeview(table_frame)
+tree["columns"] = ("ID", "Author", "Year Published", "Title", "Genre", "State")
 
 # Create headings for the table
-for heading in table["columns"]:
-    table.heading(heading, text=heading)
+for heading in tree["columns"]:
+    tree.heading(heading, text=heading)
 
 
-def get_and_insert_data():
-    data = fetch_data()
-    for item in data:
-        table.insert("", tk.END, values=item[14])
-
-
-getData = tk.Button(root, text="Get data", command=get_and_insert_data())
+getData = tk.Button(root, text="Display Data", command=view_data)
 getData.pack()
-table.pack()
+tree.pack()
 root.mainloop()
